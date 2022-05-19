38adede6db2f553cf233db7562612b966c4da298
Kamelz70 <mohamed.kamel.m73@gmail.com>
2022-05-19 19:51:22 -0400

    added first 3 choices,inde from file, file from inode, hashed bit by bit imagei

--- mini-forensics.sh
+++ mini-forensics.sh
@@ -2,50 +2,61 @@
 
 #regular expression to check with if inputs are numbers
 #starts with + or - and then any number of digits
-numRe='^[+|-]*[0-9]+$'
+re='^[+|-]*[0-9]+$'
 
-#regular expression to check with if inputs are valid operations
-operatorRe='^[+|-]{1}$'
-#Save last operation to do on result
-lastOperation=+
 
-#Result which changes over each argument
-result=0
+#loop untill a valid choice is input
+while [ 1 ]; do
 
-#Argument number to check if operation or number
-argnum=1
+echo "Enter A Choice :"
+echo "1. Find inode of a file"
+echo "2. Find file of an inode number"
+echo "3. Take a hashed bit-by-bit image of a drive"
+echo "4. Divide"
+echo "5. exit"
+#read choice
+read choice
 
-#loop on all arguments
-for arg in $@
-do
-#even argument for operations only
-if [ $(($argnum % 2)) == 0 ] ; then
-    #check argument if alid operation
-    if ! [[ $arg =~ $operatorRe ]] ; then
-        echo "error: input $arg Not a valid operation" >&2;
-        exit
-    else
-        lastOperation=$arg
-    fi
-#odd argument for numbers only
-else 
-    #check argument if alid number    
-    if ! [[ $arg =~ $numRe ]] ; then
-        echo "error: input $arg Not a number" >&2;
-        exit
-    else
-        #do operation based on argument add or sub
-        if [ $lastOperation == '-' ] ; then
-            result=$(($result - $arg)) 
-        else
-            result=$(($result + $arg)) 
-        fi
-    fi
-fi
-#increment argument number
-argnum=$(($argnum + 1))
+#execute choice
+case $choice in
+  1)
+    echo "Enter A File name or directory :"
+    read file
+    ls -i $file
+    ;;
+  2)
+    echo "Enter inode number"
+    read inodeNum
+    find -inum $inodeNum
+  ;;
+  3)mini-forensics.sh
+    echo "Enter Source File Path to copy"
+    read srcpath
+    echo ""
+    echo "Enter destination path ending with desired image name"
+    echo "Exaple: /home/kali/Desktop/imgName"
+    read dstpath
+    echo "Enter desired hashing algorithm"
+    echo "sha1, ,sha2, or md5"
+    read hashAlgo
+    echo ""
+    echo "Enter desired byte size for image"
+    read byteSize
+    echo ""
+
+    dcfldd if=$srcpath of=$dstpath hash=$hashAlgo bs=$byteSize
+
+  ;;
+  4)result=$(($n1 / $n2))
+  ;;
+  5)exit
+  ;;
+  *)
+  #redo the loop if choice incorrect
+  echo "invalid choice"
+  continue
+  ;;
+esac
+#break on correct choice
+break
 done
-
-#print result
-echo "Result is : $result"
-
