09049aaa95c80d9c9498aa0a00f34e92640ad80e
Kamelz70 <mohamed.kamel.m73@gmail.com>
2022-05-19 22:48:09 -0400

    added 2 network choices

--- mini-forensics.sh
+++ mini-forensics.sh
@@ -7,13 +7,14 @@ re='^[+|-]*[0-9]+$'
 
 #loop untill a valid choice is input
 while [ 1 ]; do
-
+echo ""
 echo "Enter A Choice :"
 echo "1. Find inode of a file"
 echo "2. Find file of an inode number"
 echo "3. Take a hashed bit-by-bit image of a drive"
-echo "4. Divide"
-echo "5. exit"
+echo "4. Capture packets from a network device"
+echo "5. Read a network trace file (.pcap)"
+echo "6. exit"
 #read choice
 read choice
 
@@ -23,36 +24,56 @@ case $choice in
     echo "Enter A File name or directory :"
     read file
     ls -i $file
+    continue
     ;;
   2)
     echo "Enter inode number"
     read inodeNum
     find -inum $inodeNum
+    continue
   ;;
-  3)mini-forensics.sh
+  3)
     echo "Enter Source File Path to copy"
     read srcpath
     echo ""
     echo "Enter destination path ending with desired image name"
     echo "Exaple: /home/kali/Desktop/imgName"
     read dstpath
-    echo "Enter desired hashing algorithm"
-    echo "sha1, ,sha2, or md5"
-    read hashAlgo
-    echo ""
     echo "Enter desired byte size for image"
     read byteSize
     echo ""
-
-    dcfldd if=$srcpath of=$dstpath hash=$hashAlgo bs=$byteSize
-
+    dcfldd if=$srcpath of=$dstpath hash=md5 bs=$byteSize
+    echo ""
+    echo "Hashes for both source and result image:"
+    md5sum $srcpath
+    md5sum $dstpath
+    continue
+    ;;
+  4)
+    echo ""
+    echo "Available network devices to choose from:"
+    sudo tcpdump -D 
+    echo ""
+    echo "Enter a device to capture packets"
+    echo "Once started press ctrl+c to stop capturing"
+    read interface
+    sudo tcpdump --interface  $interface
+    
+    continue
+    ;;
+    
+  5)
+    echo ""
+    echo "enter path to file"
+    read file
+    tcpdump -r $file 
+    continue
   ;;
-  4)result=$(($n1 / $n2))
-  ;;
-  5)exit
+  6)exit
   ;;
   *)
   #redo the loop if choice incorrect
+  echo ""
   echo "invalid choice"
   continue
   ;;
